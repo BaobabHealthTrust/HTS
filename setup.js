d24bcfa7dcc6cd6e1069a65923a759b4aa1a3b66
cranberry <manzymkamba@gmail.com>
2016-11-24 16:06:37 +0200

    Setup create  or updates public/config files

--- setup.js
+++ setup.js
@@ -15,6 +15,49 @@ function runCmd(cmd, callBack) {
     });
 }
 
+function updatedHash(hash1, hash2){
+
+        if(Object.keys(hash2) === undefined){
+
+            hash2 = hash1 ;
+
+            return;
+
+        }
+
+        var keys = Object.keys(hash1);
+
+        for(var i = 0 ; i < keys.length ; i++){
+
+            var key = keys[i];
+
+            if(hash2[key]){
+
+                if (typeof hash1[key] == typeof {}) {
+
+                        updatedHash(hash1[key], hash2[key])
+
+                }else{
+
+                        if(hash2[key]){
+
+                         }else{
+
+                                hash2[key] = hash1[key]
+                                
+                         }                                 
+                }
+
+            }
+            else{
+
+                hash2[key] = hash1[key];
+            }
+
+        }
+
+}
+
 var rootPath = path.resolve(settings.common_folder_path);
 
 async.series([
@@ -338,9 +381,116 @@ async.series([
 
         }
 
+    },
+    function(callback){
+
+            fs.readdir(rootPath + "/modules/config/",function(err, files){
+
+               if (err) {
+
+                  return console.error(err);
+
+               }
+
+               files.forEach( function (file){
+
+                        if(file =="patient.modules.json.example" || file =="landing.modules.json.example" || file.match("~")){
+
+                            return;
+
+                        }
+
+                        async.series([
+
+                                        function(callback){
+
+                                                if (fs.existsSync(path.resolve("./public/config/"+file)) && !file.match("~")){
+
+                                                     var imports =  JSON.parse(fs.readFileSync(rootPath + "/modules/config/"+file));
+
+                                                     var local =  JSON.parse(fs.readFileSync(path.resolve("./public/config/"+file)));
+
+                                                     var local_locked =  JSON.parse(fs.readFileSync(path.resolve("./public/config/"+file)));
+
+                                                     async.series([
+                                                                        function(callback){
+
+                                                                            updatedHash(imports, local);
+
+                                                                            callback()
+
+                                                                        },
+
+                                                                        function(callback){
+
+                                                                            var local_locked =  JSON.parse(fs.readFileSync(path.resolve("./public/config/"+file)));
+
+                                                                            if(JSON.stringify(local) === JSON.stringify(local_locked)){
+
+                                                                                    console.log("File OK "+file)
+
+                                                                            }else{
+
+                                                                                    fs.writeFile(path.resolve("./public/config/"+file), JSON.stringify(local), function(error){
+
+                                                                                        if(error){
+
+                                                                                                console.log(error);
+
+                                                                                        }else{
+
+                                                                                                console.log("File Update "+file);
+                                                                                        }
+
+                                                                                    })
+
+                                                                            }
+
+                                                                            callback();
+
+                                                                        }
+
+                                                        ]);
+
+                                                    }else{
+
+                                                        runCmd("cp " + rootPath + "/modules/config/"+file+" ./public/config/"+file, function (error, stdout, stderr) {
+
+                                                                console.log("Copy "+file);
+                                                                if (error) {
+
+                                                                    console.log(error);
+
+                                                                } else if (stderr) {
+
+                                                                    console.log(stderr);
+
+                                                                } else if (stdout) {
+
+                                                                    console.log(stdout);
+
+                                                                }
+
+                                                         })
+
+                                                }
+
+                                                callback();
+
+                                        }
+
+                            ]);
+
+               });
+
+            });
+
+        callback();
+
     }
 
-], function () {
+],
+function () {
 
     console.log("All set!");
 
