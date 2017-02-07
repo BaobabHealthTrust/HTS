102f160124e427c6de3f24b5e41b538dd5dab054
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2017-02-07 15:10:35 +0200

    Added logic for disabling some options not relevant on adding stock

--- public/javascripts/stock_custom.js
+++ public/javascripts/stock_custom.js
@@ -1,63 +1,124 @@
-function checkFirstSeecondTestAvailable(){
+function checkFirstSecondTestAvailable() {
 
-        var descriptions = ['First Test','Second Test']
+    var descriptions = ['First Test', 'Second Test']
 
-        var available = {};
+    var available = {};
 
-        for(var i = 0 ; i < descriptions.length ; i++){
+    for (var i = 0; i < descriptions.length; i++) {
 
-            window.parent.stock.ajaxRequest("/stock/available_kits_by_desctiption/"+ encodeURIComponent(descriptions[i]), function(data) {
+        window.parent.stock.ajaxRequest("/stock/available_kits_by_desctiption/" + encodeURIComponent(descriptions[i]), function (data) {
 
-                    if(!data)
-                        var data = {};
+            if (!data)
+                var data = {};
 
-                    var json = (typeof data == typeof String() ? JSON.parse(data) : data)
+            var json = (typeof data == typeof String() ? JSON.parse(data) : data)
 
-                    if(json.name){
+            if (json.name) {
 
-                            available[json.description] = {name: json.name , present: true};
+                available[json.description] = {name: json.name, present: true};
 
-                    }
-                    else{
+            } else {
 
-                            available[json.description] = {name: json.name , present: false};
+                available[json.description] = {name: json.name, present: false};
 
-                    }
+            }
 
 
-            });
+        });
 
-        }
+    }
 
-        window.parent.stock.availableByDescription = available;
-
+    window.parent.stock.availableByDescription = available;
 
 }
 
-function validateDescription(){
+function validateDescription() {
 
-       var description = __$('touchscreenInput' + tstCurrentPage).value.trim();
+    var description = __$('touchscreenInput' + tstCurrentPage).value.trim();
 
-       var available = window.parent.stock.availableByDescription;
+    var available = window.parent.stock.availableByDescription;
 
-       if(available[description]){
+    if (available[description]) {
 
-                 setTimeout(function(){
+        setTimeout(function () {
 
-                            gotoPage(tstCurrentPage - 1, false, true);
+            gotoPage(tstCurrentPage - 1, false, true);
 
+            window.parent.stock.showMsg(available[description].name + ' is already set as ' + description, "Item with description already exists");
 
-                            window.parent.stock.showMsg(available[description].name + ' is already set as '+ description, "Item with description already exists");
+            var ok = window.parent.stock.$("ok_button");
 
-                            var ok = window.parent.stock.$("ok_button");
+            ok.setAttribute("onclick", 'window.parent.stock.listItems(window.parent.document.body)');
 
-                            ok.setAttribute("onclick",'window.parent.stock.listItems(window.parent.document.body)');
+        }, 10);
 
+    }
 
-                        },
+}
 
-                 10);
+function reloadDescriptions() {
 
-       }
+    if(__$('touchscreenInput' + tstCurrentPage).value.match(/dts/i)) {
+
+        if(__$("data.description")) {
+
+            var options = __$("data.description").getElementsByTagName("option");
+
+            for(var i = options.length - 1; i >= 0; i--) {
+
+                var option = options[i];
+
+                if(['First Test','Second Test'].indexOf(option.innerHTML.trim()) >= 0) {
+
+                    __$("data.description").removeChild(options[i]);
+
+                }
+
+            }
+
+        }
+
+    } else {
+
+        if(__$("data.description")) {
+
+            var options = __$("data.description").getElementsByTagName("option");
+
+            var labels = ['First Test','Second Test'];
+
+            var options = __$("data.description").getElementsByTagName("option");
+
+            var found = false;
+
+            for(var i = 0; i < options.length; i++) {
+
+                var option = options[i];
+
+                if(labels.indexOf(option.innerHTML.trim()) >= 0) {
+
+                    found = true;
+
+                    break;
+
+                }
+
+            }
+
+            if(!found) {
+
+                for (var i = 0; i < labels.length; i++) {
+
+                    var opt = document.createElement("option");
+                    opt.innerHTML = labels[i];
+
+                    __$("data.description").appendChild(opt);
+
+                }
+
+            }
+
+        }
+
+    }
 
 }
